adbb5f930875f7bb619d979057118cb89c9676f0
Vegard Øye <vegard_oye@hotmail.com>
2016-12-24 08:15:57 +0100

    Updated spotify

--- spotify/web.js
+++ spotify/web.js
@@ -1,8 +1,9 @@
-var spotify = require('../spotify')
+var Playlist = require('../lib/playlist')
 
 document.addEventListener('DOMContentLoaded', function () {
   var form = document.querySelector('form')
   var textarea = document.querySelector('textarea')
+  var button = document.querySelector('button')
   var log = document.querySelector('.log')
 
   console.log = function (message) {
@@ -12,12 +13,21 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   form.onsubmit = function () {
-    var playlist = new spotify.Playlist(textarea.value)
+    var playlist = new Playlist(textarea.value)
+    button.innerHTML = 'Creating Playlist &hellip;'
+    button.classList.add('active')
+    button.classList.add('disabled')
     playlist.dispatch().then(function (result) {
       console.log('')
+      button.classList.remove('disabled')
       textarea.value = result
       textarea.focus()
       textarea.select()
+      if (result !== '') {
+        button.innerHTML = 'Created Playlist'
+        console.log('Copy and paste the above ' +
+                    'into a new Spotify playlist')
+      }
     })
     return false
   }
